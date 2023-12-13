cc75c4cab27401009c2192e86a965771db0c8cfb
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2023-12-13 21:08:18 +0530

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,40 +1,40 @@
-ESX = exports['es_extended']:getSharedObject()
+local QBCore = exports['qb-core']:GetCoreObject()
 
-ESX.RegisterServerCallback('JG-DrugAddiction:getdrugconsumption', function(src, cb)
-   local xPlayer = ESX.GetPlayerFromId(src)
-   if xPlayer then
-    MySQL.scalar('SELECT drugconsumption FROM users WHERE identifier = ?', {xPlayer.identifier}, function(drugconsumption)
-    cb(drugconsumption)
-    end)
+-- Callback for getting drug consumption
+QBCore.Functions.CreateCallback('JG-DrugAddiction:getdrugconsumption', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player then
+        -- Assuming your database structure is the same, otherwise adjust accordingly
+        MySQL.scalar('SELECT drugconsumption FROM players WHERE identifier = ?', {Player.PlayerData.citizenid}, function(drugconsumption)
+            cb(drugconsumption)
+        end)
     end
 end)
 
-ESX.RegisterServerCallback('JG-DrugAddiction:adddrugconsumption', function(src)
-    local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer then
-        MySQL.update('UPDATE users SET drugconsumption = drugconsumption + 1 WHERE identifier = ?', {xPlayer.identifier})
-     end
- end)
- 
+-- Callback for adding drug consumption
+QBCore.Functions.CreateCallback('JG-DrugAddiction:adddrugconsumption', function(source)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player then
+        MySQL.update('UPDATE players SET drugconsumption = drugconsumption + 1 WHERE identifier = ?', {Player.PlayerData.citizenid})
+    end
+end)
 
-
-for k,v in pairs(Config.items) do 
-    ESX.RegisterUsableItem(k, function(playerId)
-       local xPlayer = ESX.GetPlayerFromId(playerId)
-       xPlayer.triggerEvent('JG-DrugAddiction:encrementdrug')
-       xPlayer.triggerEvent('JG-DrugAddiction:reducedesire')
+-- Registering usable items from a config (adjust the Config.items accordingly)
+for k, v in pairs(Config.items) do 
+    QBCore.Functions.CreateUseableItem(k, function(playerId)
+        local Player = QBCore.Functions.GetPlayer(playerId)
+        TriggerClientEvent('JG-DrugAddiction:incrementdrug', playerId) -- Adjust the event names as per your client script
+        TriggerClientEvent('JG-DrugAddiction:reducedesire', playerId)
     end)
 end   
 
-
-
-ESX.RegisterUsableItem('cureitem', function(playerId)
-   local xPlayer = ESX.GetPlayerFromId(playerId)
-   xPlayer.triggerEvent('JG-DrugAddiction:Cure')
-   xPlayer.removeInventoryItem('cureitem', 1)
-   xPlayer.showNotification('You are cured') 
-   if xPlayer then
-    MySQL.update('UPDATE users SET drugconsumption = 0 WHERE identifier = ?', {xPlayer.identifier})
-   end
+-- Using the cure item
+QBCore.Functions.CreateUseableItem('cureitem', function(playerId)
+    local Player = QBCore.Functions.GetPlayer(playerId)
+    if Player then
+        TriggerClientEvent('JG-DrugAddiction:Cure', playerId)
+        Player.Functions.RemoveItem('cureitem', 1)
+        TriggerClientEvent('QBCore:Notify', playerId, 'You are cured', 'success') 
+        MySQL.update('UPDATE players SET drugconsumption = 0 WHERE identifier = ?', {Player.PlayerData.citizenid})
+    end
 end)
-
